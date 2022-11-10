71b333ce37f458deecedf80491c16b939d00686b
James Li <jamesli1218@gmail.com>
2022-11-10 11:32:54 -0500

    adding search

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import './App.css';
 import Search from './components/search/search';
 
 function App() {
+
+const handleOnSearchChange = (searchData) => {
+  console.log(searchData);
+}
+
   return (
     <div class="container">
-      <Search />
+      <Search onSearchChange={handleOnSearchChange}/>
     </div>
   );
 }
---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1fea2c3859mshb3eca0708757783p1ff100jsnb84ce98f5c0f',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
