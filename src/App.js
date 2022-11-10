493e3962292f752cb4a2ffd832d28cc69ec0f3f9
James Li <jamesli1218@gmail.com>
2022-11-10 12:49:24 -0500

    integrated city api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Search from './components/search/search';
+import CurrentWeather from './components/current-weather/current-weather';
 
 function App() {
 
@@ -10,6 +11,7 @@ const handleOnSearchChange = (searchData) => {
   return (
     <div class="container">
       <Search onSearchChange={handleOnSearchChange}/>
+      <CurrentWeather />
     </div>
   );
 }
