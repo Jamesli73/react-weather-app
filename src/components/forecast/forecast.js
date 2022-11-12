87a99a52969e7ceff555fa3b1bfdc2c2d1f1702e
James Li <j2668li@uwaterloo.ca>
2022-11-12 10:53:48 -0500

    added forecast

---
+++ src/components/forecast/forecast.js
@@ -0,0 +1,29 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemButton,
+  AccordionItemHeading,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+
+const Forecast = ({ data }) => {
+  return (
+    <>
+      <label className="title">Daily</label>
+      <Accordion allowZeroExpanded>
+        {DataTransfer.list.splice(0, 7).map((item, idx) => (
+          <AccordionItem key={idx}>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                Hello
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel></AccordionItemPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </>
+  );
+};
+
+export default Forecast;
